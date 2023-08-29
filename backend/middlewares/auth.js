1bd148570ee62ca4e6801dd37c512dfbee374388
Karina Savilova <savilovakarina@yandex.ru>
2023-08-29 19:42:34 +0200

    test 21

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // const { SECRET_KEY } = process.env;
-const { SECRET_KEY } = require('../utils/config');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+const { SECRET_KEY } = require("../utils/config");
+const UnauthorizedError = require("../errors/UnauthorizedError");
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Необходима авторизация"));
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (error) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError("Необходима авторизация");
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
